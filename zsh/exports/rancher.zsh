bb8fea414654e9d41f62e44d1af3d2f8fed5efc4
Eduardo Bizarro <edbizarro@gmail.com>
2018-04-04 14:36:26 -0300

    remove rancher file

--- zsh/exports/rancher.zsh
+++
@@ -1,6 +0,0 @@
-# Set the url that Rancher is on
-export RANCHER_URL='https://mystery-machine.convenia.com.br'
-# Set the access key, i.e. username
-export RANCHER_ACCESS_KEY='DF7365F632BC65160626'
-# Set the secret key, i.e. password
-export RANCHER_SECRET_KEY='ZXEBHBiQ25sTgmZ58cwYXP3SReav2RSfZkFBnRBL'
