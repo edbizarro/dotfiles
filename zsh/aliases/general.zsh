d83561aabebb893a94230d8e58a4441ed6e4c742
Eduardo Bizarro <edbizarro@gmail.com>
2019-04-03 22:59:35 -0300

    alias and git config

--- zsh/aliases/general.zsh
+++ zsh/aliases/general.zsh
@@ -9,3 +9,5 @@ alias reboot='sudo systemctl reboot'
 alias shutdown='sudo systemctl poweroff'
 
 alias monitor_disk='sudo iotop -Pao'
+
+alias hdmi='xrandr --dpi 96 --output HDMI-1  --primary && mons -o'
