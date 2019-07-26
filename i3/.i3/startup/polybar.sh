7e45d14887846c204b893d4cedb6af00f54887ad
Eduardo Bizarro <edbizarro@gmail.com>
2019-07-26 01:36:18 -0300

    Better polybar startup script
    Thanks to https://github.com/polybar/polybar/issues/763#issuecomment-392960721

--- i3/.i3/startup/polybar.sh
+++ i3/.i3/startup/polybar.sh
@@ -6,6 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
-polybar bottom -r&
-polybar top -r&
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload bottom -r&
+    MONITOR=$m polybar --reload top -r&
+done
+
