43e6a161c07517ff163ed9fb38a5e55a9b801321
Eduardo Bizarro <edbizarro@gmail.com>
2020-03-29 17:05:31 -0300

    scripts

--- i3/.i3/startup/compton.sh
+++ i3/.i3/startup/compton.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 # Terminate already running bar instances
-killall -q compton
+killall -q picom
 
 # Wait until the processes have been shut down
-while pgrep -x compton >/dev/null; do sleep 1; done
+while pgrep -x picom >/dev/null; do sleep 1; done
 
 # Launch compton
-compton --config  ~/.config/compton.conf -b &
+picom --config  ~/.config/compton.conf -b &
--- i3/.i3/startup/unclutter.sh
+++ i3/.i3/startup/unclutter.sh
@@ -6,5 +6,5 @@ killall -q unclutter
 # Wait until the processes have been shut down
 while pgrep -x unclutter >/dev/null; do sleep 1; done
 
-# Launch compton
+# Launch unclutter
 unclutter &
