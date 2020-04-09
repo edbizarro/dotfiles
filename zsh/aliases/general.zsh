8cec0ca1f1a2f8ecd5e079ea9fb85e958cbe4b00
Eduardo Bizarro <edbizarro@gmail.com>
2020-04-09 15:26:21 -0300

    aliases

--- zsh/aliases/general.zsh
+++ zsh/aliases/general.zsh
@@ -17,3 +17,6 @@ alias sudo="sudo "
 
 alias k-abnt2="setxkbmap -model abnt2 -layout br -variant abnt2"
 alias k-us="setxkbmap -model us -layout us"
+
+alias desktop-mode="k-abnt2 && xrandr --dpi 96 --output DP1  --primary && mons -o && light -A 100"
+alias laptop-mode="k-us && light -U 30"
