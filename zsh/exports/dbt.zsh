d655c9d835bdb3e02c627fe340097d55d8b1ea9c
Eduardo Bizarro <edbizarro@gmail.com>
2020-06-03 11:14:05 -0300

    aliases

---
+++ zsh/exports/dbt.zsh
@@ -0,0 +1,6 @@
+function dbt_run_changed() {
+    children=$1
+    models=$(git diff --name-only | grep '\.sql$' | awk -F '/' '{ print $NF }' | sed "s/\.sql$/${children}/g" | tr '\n' ' ')
+    echo "Running models: ${models}"
+    dkc exec dbt dbt run --models $models
+}
