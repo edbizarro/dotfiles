847f8c2eec48d3982b41cc0a379ba91c1a103668
Eduardo Bizarro <edbizarro@gmail.com>
2020-10-21 17:26:30 -0300

    configs

--- i3/.i3/startup/polybar.sh
+++ i3/.i3/startup/polybar.sh
@@ -6,8 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload bottom -r&
-    MONITOR=$m polybar --reload top -r&
-done
+polybar --reload bottom -r&
+polybar --reload top -r&
 
