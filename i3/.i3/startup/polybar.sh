bbd1138c57eeefd722a39a34f988a2b265a411af
Eduardo Bizarro <edbizarro@gmail.com>
2021-10-02 19:48:40 -0300

    configs

--- i3/.i3/startup/polybar.sh
+++ i3/.i3/startup/polybar.sh
@@ -1,11 +1,10 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-polybar --reload bottom -r&
-polybar --reload top -r&
-
+polybar bottom -r 2>&1 | tee -a /tmp/polybar.log & disown
+polybar top -r 2>&1 | tee -a /tmp/polybar.log 2& disown
