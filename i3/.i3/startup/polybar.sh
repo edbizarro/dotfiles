2e6774e578a3c0403844f184ba3eb89820643c03
Eduardo Bizarro <edbizarro@gmail.com>
2021-10-22 15:05:53 -0300

    configs

--- i3/.i3/startup/polybar.sh
+++ i3/.i3/startup/polybar.sh
@@ -6,5 +6,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-polybar bottom -r 2>&1 | tee -a /tmp/polybar.log & disown
-polybar top -r 2>&1 | tee -a /tmp/polybar.log & disown
+polybar bottom -r &
+polybar top -r &
