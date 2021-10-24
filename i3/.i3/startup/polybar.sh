a408f14184e7ff32c40bfa9f9a7f2356fd27c979
Eduardo Bizarro <edbizarro@gmail.com>
2021-10-23 22:32:32 -0300

    fix polybar init script

--- i3/.i3/startup/polybar.sh
+++ i3/.i3/startup/polybar.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
 polybar bottom -r &
 polybar top -r &
