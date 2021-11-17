ba646be4f58c8888045c5b0b3bd77e574de5f90d
Eduardo Bizarro <edbizarro@gmail.com>
2021-11-17 10:23:33 -0300

    configs

--- zsh/aliases/dbt.zsh
+++ zsh/aliases/dbt.zsh
@@ -2,7 +2,7 @@ function dbt_run_changed() {
     children=$1
     models=$(git diff --name-only | grep '\.sql$' | awk -F '/' '{ print $NF }' | sed "s/\.sql$/${children}/g" | tr '\n' ' ')
     echo "Running models: ${models}"
-    dkc exec dbt dbt run --models $models
+    dbt run --models $models
 }
 
 function cycle_logs() {
--- zsh/aliases/general.zsh
+++ zsh/aliases/general.zsh
@@ -17,12 +17,10 @@ alias shutdown='sudo systemctl poweroff'
 alias monitor_disk='sudo iotop -Pao'
 alias disk-details="ncdu / --exclude ~/.avfs --exclude /media"
 
-alias hdmi='xrandr --dpi 96 --output HDMI-1  --primary && mons -o'
-
 alias sudo="sudo "
 
 alias k-abnt2="setxkbmap -model abnt2 -layout br -variant abnt2"
 alias k-us="setxkbmap -model us -layout us -variant intl"
 
-alias desktop-mode="k-us && xrandr --dpi 192 --output DP-1 --primary && mons -o && light -A 100"
-alias laptop-mode="k-us && xrandr --dpi 192 --output eDP-1 --primary && light -S 70"
+alias desktop-mode="k-us && xrandr --output HDMI-0 --primary --mode 3440x1440 --rate 100 && mons -o && light -A 100"
+alias laptop-mode="k-us && xrandr --output eDP-1-1 --primary && light -S 70"
